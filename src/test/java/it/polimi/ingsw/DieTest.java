7519063ffbb082f53ec8be269e739f4c88c5f304
Fabio Codiglioni <fabiocody@icloud.com>
2018-04-27 19:24:13 +0200

    Correct tests

--- src/test/java/it/polimi/ingsw/DieTest.java
+++ src/test/java/it/polimi/ingsw/DieTest.java
@@ -1,37 +1,38 @@
 package it.polimi.ingsw;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import it.polimi.ingsw.util.Colors;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import it.polimi.ingsw.dice.Die;
-import it.polimi.ingsw.dice.DiceGenerator;
+import it.polimi.ingsw.dice.*;
+
 
 public class DieTest {
 
-    private int value;
-    private Colors color;
+    private static DiceGenerator generator;
+
+    @BeforeAll
+    static void setup() {
+        generator = new DiceGenerator(4);
+    }
 
 
     @Test
     public void valueTest() {
-        Die testDie = new Die();
-        value = testDie.getValue();
-        assertTrue(value>0 && value <=6);
+        Die d = generator.generate();
+        assertTrue(d.getValue() >= 1 && d.getValue() <= 6);
     }
 
     @Test
     public void colorTest() {
-        Die testDie = new Die();
-        color = testDie.getColor();
-        assertTrue(color.equals(Colors.RED) ^ color.equals(Colors.GREEN) ^ color.equals(Colors.YELLOW) ^
-                color.equals(Colors.BLUE) ^ color.equals(Colors.PURPLE));
+        Die d = generator.generate();
+        Colors color = d.getColor();
+        assertTrue(color.equals(Colors.RED) || color.equals(Colors.GREEN) || color.equals(Colors.YELLOW) || color.equals(Colors.BLUE) || color.equals(Colors.PURPLE));
     }
 
     @Test
     public void generateTest() {
-        DiceGenerator diceGenerator = new DiceGenerator(4);
-        Die testDie = diceGenerator.generate();
-        assertNotNull(testDie);
+        Die d = generator.generate();
+        assertNotNull(d);
     }
 }
