556e31c126e9684c77e0326187e39914e3087407
Fabio Codiglioni <fabiocody@icloud.com>
2018-05-04 13:05:22 +0200

    Update main

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -1,19 +1,8 @@
 package it.polimi.ingsw;
 
-import  it.polimi.ingsw.patterncards.*;
-import java.util.List;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
+public class App {
     public static void main( String[] args ) {
-        PatternCardsGenerator a = new PatternCardsGenerator();
-        List <WindowPattern> b = a.getCards();
-
-        for(WindowPattern c: b)
-            c.dump();
+        System.out.println("Project still under development");
     }
 }
