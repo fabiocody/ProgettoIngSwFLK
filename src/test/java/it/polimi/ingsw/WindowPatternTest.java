b480159f6e83bcee49b71bc6cc65f1150a885a1f
LucaDLL <lucadelloglio@hotmail.it>
2018-05-07 21:40:45 +0200

    Added OrthogonalConstraintTest

--- src/test/java/it/polimi/ingsw/WindowPatternTest.java
+++ src/test/java/it/polimi/ingsw/WindowPatternTest.java
@@ -9,7 +9,6 @@ import it.polimi.ingsw.placementconstraints.*;
 import it.polimi.ingsw.util.Colors;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.List;
 
@@ -89,9 +88,9 @@ class WindowPatternTest {
     void PlaceDieEmptyConstraintTest(){
         PlacementConstraint con = new EmptyConstraint();
         WindowPattern pattern = new WindowPattern(42);  //default pattern
-        Die d1 = new Die(Colors.getRandomColor(),ThreadLocalRandom.current().nextInt(1,6));
-        Die d2 = new Die(Colors.getRandomColor(),ThreadLocalRandom.current().nextInt(1,6));
-        int index = ThreadLocalRandom.current().nextInt(0,19);
+        Die d1 = new Die(Colors.getRandomColor(),ThreadLocalRandom.current().nextInt(1,7));
+        Die d2 = new Die(Colors.getRandomColor(),ThreadLocalRandom.current().nextInt(1,7));
+        int index = ThreadLocalRandom.current().nextInt(0,20);
         pattern.placeDie(d1,index,con);
         assertTrue(pattern.getCellAt(index).getPlacedDie() == d1);
         assertThrows(InvalidPlacementException.class,
@@ -104,12 +103,12 @@ class WindowPatternTest {
     @Test
     void BorderConstraintTest(){
         PlacementConstraint con = new BorderConstraint(new EmptyConstraint());
-        Die d = new Die(Colors.getRandomColor(),ThreadLocalRandom.current().nextInt(1,6));
+        Die d = new Die(Colors.getRandomColor(),ThreadLocalRandom.current().nextInt(1,7));
         for(int i = 0; i < 2; i++){
             WindowPattern pattern = new WindowPattern(42);                      //default pattern
             WindowPattern pattern2 = new WindowPattern(42);
-            int index = ThreadLocalRandom.current().nextInt(0,4) + 15*i;
-            int index2 = 5*ThreadLocalRandom.current().nextInt(0,3) + 4*i;
+            int index = ThreadLocalRandom.current().nextInt(0,5) + 15*i;
+            int index2 = 5*ThreadLocalRandom.current().nextInt(0,4) + 4*i;
             pattern.placeDie(d,index,con);
             pattern2.placeDie(d,index2,con);
             assertTrue(pattern.getCellAt(index).getPlacedDie() != null);
@@ -120,13 +119,13 @@ class WindowPatternTest {
     @Test
     void ColorConstraintTest(){
         PlacementConstraint con = new ColorConstraint(new EmptyConstraint());
-        WindowPattern pattern = new WindowPattern(ThreadLocalRandom.current().nextInt(0,23));
+        WindowPattern pattern = new WindowPattern(ThreadLocalRandom.current().nextInt(0,24));
         int j;
         do{
-            j = ThreadLocalRandom.current().nextInt(0,19);
+            j = ThreadLocalRandom.current().nextInt(0,20);
         } while (pattern.getCellAt(j).getCellColor() == null);
 
-        Die d = new Die(pattern.getCellAt(j).getCellColor(),ThreadLocalRandom.current().nextInt(1,6));
+        Die d = new Die(pattern.getCellAt(j).getCellColor(),ThreadLocalRandom.current().nextInt(1,7));
         pattern.placeDie(d,j,con);
         assertTrue(pattern.getCellAt(j).getCellColor() == d.getColor());
     }
@@ -134,10 +133,10 @@ class WindowPatternTest {
     @Test
     void ValueConstraintTest(){
         PlacementConstraint con = new ValueConstraint(new EmptyConstraint());
-        WindowPattern pattern = new WindowPattern(ThreadLocalRandom.current().nextInt(0,23));
+        WindowPattern pattern = new WindowPattern(ThreadLocalRandom.current().nextInt(0,24));
         int j;
         do{
-            j = ThreadLocalRandom.current().nextInt(0,19);
+            j = ThreadLocalRandom.current().nextInt(0,20);
         } while (pattern.getCellAt(j).getCellValue() == null);
         Die d = new Die(Colors.getRandomColor(),pattern.getCellAt(j).getCellValue());
         pattern.placeDie(d,j,con);
@@ -148,10 +147,10 @@ class WindowPatternTest {
     void PositionConstraintTest(){
         PlacementConstraint con1 = new EmptyConstraint();
         PlacementConstraint con2 = new PositionConstraint(new EmptyConstraint());
-        int index = ThreadLocalRandom.current().nextInt(0,19);
+        int index = ThreadLocalRandom.current().nextInt(0,20);
         List<Integer> list = Constraint.validPositions(index);
-        Die d1 = new Die(Colors.getRandomColor(),ThreadLocalRandom.current().nextInt(1,6));
-        Die d2 = new Die(Colors.getRandomColor(),ThreadLocalRandom.current().nextInt(1,6));
+        Die d1 = new Die(Colors.getRandomColor(),ThreadLocalRandom.current().nextInt(1,7));
+        Die d2 = new Die(Colors.getRandomColor(),ThreadLocalRandom.current().nextInt(1,7));
         for(int i: list){
             WindowPattern pattern = new WindowPattern(42);
             pattern.placeDie(d1, index,con1);
@@ -160,28 +159,23 @@ class WindowPatternTest {
         }
     }
 
+    @Test
     void OrthogonalConstraintTest(){
         PlacementConstraint con1 = new EmptyConstraint();
         PlacementConstraint con2 = new OrthogonalConstraint(new EmptyConstraint());
-        int index = ThreadLocalRandom.current().nextInt(0,19);
+        int index = ThreadLocalRandom.current().nextInt(0,20);
         Colors color = Colors.getRandomColor();
-        int dieValue = ThreadLocalRandom.current().nextInt(1,6);
+        int dieValue = ThreadLocalRandom.current().nextInt(1,7);
         List<Integer> list = Constraint.validOrthogonalPositions(index);
-        Die d1 = new Die(color,ThreadLocalRandom.current().nextInt(1,6));
-        Die d2 = new Die(color,ThreadLocalRandom.current().nextInt(1,6));
+        Die d1 = new Die(color,ThreadLocalRandom.current().nextInt(1,7));
+        Die d2 = new Die(color,ThreadLocalRandom.current().nextInt(1,7));
         for(int i: list){
             WindowPattern pattern = new WindowPattern(42);
             pattern.placeDie(d1, index,con1);
-            pattern.placeDie(d2,i,con2);
-            assertTrue(pattern.getCellAt(i).getPlacedDie() == d2);
-        }
-        d1 = new Die(Colors.getRandomColor(),dieValue);
-        d2 = new Die(Colors.getRandomColor(),dieValue);
-        for(int i: list){
-            WindowPattern pattern = new WindowPattern(42);
-            pattern.placeDie(d1, index,con1);
-            pattern.placeDie(d2,i,con2);
-            assertTrue(pattern.getCellAt(i).getPlacedDie() == d2);
+            assertThrows(InvalidPlacementException.class,
+                    ()->{
+                        pattern.placeDie(d2,i,con2);
+                    });
         }
     }
 }
