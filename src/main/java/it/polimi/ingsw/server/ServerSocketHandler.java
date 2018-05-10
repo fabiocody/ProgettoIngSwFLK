58f5ec9eea0da853c0680f7dfac382bdd55ab781
kaidegast <degastkai@gmail.com>
2018-05-10 12:13:05 +0200

    added ServerSocketHandler

---
+++ src/main/java/it/polimi/ingsw/server/ServerSocketHandler.java
@@ -0,0 +1,32 @@
+package it.polimi.ingsw.server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ServerSocketHandler implements Runnable {
+
+    private Socket socket;
+
+    public ServerSocketHandler(Socket socket) {
+        this.socket=socket;
+    }
+
+    @Override
+    public void run() {
+        Scanner in;
+        PrintWriter out;
+
+        try {
+            in = new Scanner(socket.getInputStream());
+            out = new PrintWriter(socket.getOutputStream(), true);
+            //TODO BODY
+            in.close();
+            out.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
