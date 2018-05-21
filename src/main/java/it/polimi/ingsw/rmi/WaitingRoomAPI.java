6aaf2617ce63cd8811118a79e2b6a6a2b33a0c27
kaidegast <degastkai@gmail.com>
2018-05-21 09:30:01 +0200

    added javaDoc to WaitingRoomAPI

--- src/main/java/it/polimi/ingsw/rmi/WaitingRoomAPI.java
+++ src/main/java/it/polimi/ingsw/rmi/WaitingRoomAPI.java
@@ -5,14 +5,69 @@ import java.rmi.*;
 import java.util.*;
 
 
+/**
+ * This is the shared interface that describes the methods that the client can call on the waiting room
+ *
+ * @author Team
+ */
 public interface WaitingRoomAPI extends Remote {
 
-    // WaitingRoom
+    /**
+     * This method is used when a client wants to join the waiting room
+     *
+     * @param nickname the nickname inserted by the player
+     * @return UUID the secret identifier used to uniquely authenticate the player
+     * @throws RemoteException required by RMI
+     * @throws LoginFailedException thrown when the player inserts a nickname that is already present on the sever
+     * side
+     */
     UUID addPlayer(String nickname) throws RemoteException, LoginFailedException;
+
+    /**
+     * This method is used to remove a player from the waiting room
+     *
+     * @param nickname the nickname of the player that has to leave the waiting room
+     * @throws RemoteException required by RMI
+     */
     void removePlayer(String nickname) throws RemoteException;
+
+    /**
+     * @return a list of the players currently logged in the waiting room
+     * @throws RemoteException required by RMI
+     */
     List<Player> getWaitingPlayers() throws RemoteException;
+
+    /**
+     * This method is used to subscribe a player to the waiting room timer, so that the client will receive regular
+     * updates
+     *
+     * @param observer the client that wants to be subscribed
+     * @throws RemoteException required by RMI
+     */
     void subscribeToWaitingRoomTimer(Observer observer) throws RemoteException;
+
+    /**
+     * This method is used to unsubscribe a player from the waiting room timer
+     *
+     * @param observer the client that no longer wants to be subscribed
+     * @throws RemoteException required by RMI
+     */
     void unsubscribeFromWaitingRoomTimer(Observer observer) throws RemoteException;
+
+    /**
+     * This method is used to subscribe a player to the waiting room, so that the client will receive regular
+     * updates
+     *
+     * @param observer the client that wants to be subscribed
+     * @throws RemoteException required by RMI
+     */
     void subscribeToWaitingRoom(Observer observer) throws RemoteException;
+
+    /**
+     * This method is used to unsubscribe a player from the waiting room timer
+     *
+     * @param observer the client that no longer wants to be subscribed
+     * @throws RemoteException required by RMI
+     */
     void unsubscribeFromWaitingRoom(Observer observer) throws RemoteException;
 }
