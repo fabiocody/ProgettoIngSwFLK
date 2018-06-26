f1d236e399122827f2563c3e87ef3ff246ebadfa
Fabio Codiglioni <fabiocody@icloud.com>
2018-06-26 17:58:16 +0200

    Fix wrong constants

--- src/main/java/it/polimi/ingsw/shared/util/Constants.java
+++ src/main/java/it/polimi/ingsw/shared/util/Constants.java
@@ -16,7 +16,7 @@ public class Constants {
     public static final int NUMBER_OF_PATTERN_ROWS = 4;
     public static final int NUMBER_OF_PATTERNS = 24;
     public static final int TOOL_CARD_NUMBER = 3;
-    public static final int NUMBER_OF_ROUNDS = 2;
+    public static final int NUMBER_OF_ROUNDS = 10;
     public static final String EXIT_MESSAGE = "Premi 0 per annullare.";
     public static final int DEFAULT_PORT = 42000;
     public static final int DEFAULT_RMI_PORT = 1099;
@@ -24,7 +24,7 @@ public class Constants {
     public static final int DEFAULT_GAME_TIMEOUT = 90;
     public static final int EXIT_STATUS = 0;
     public static final int EXIT_ERROR = 1;
-    public static final long PROBE_TIMEOUT = 300;
+    public static final long PROBE_TIMEOUT = 3;
     public static final String SERVER_RMI_NAME = "SagradaServer";
     public static final String SERVER_USAGE_STRING = "usage: sagradaserver host [--debug] [--port PORT] [--wr-timeout WR-TIMEOUT] [--game-timeout GAME-TIMEOUT]";
     public static final String CLIENT_USAGE_STRING = "usage: sagradaclient --host HOST [--debug] [--port PORT] [--connection socket|rmi] [--interface cli|gui]";
