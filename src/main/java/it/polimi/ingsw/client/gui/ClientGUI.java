3d23bc256de6d0f3c8dda2b20acf218c05fa3b6a
Fabio Codiglioni <fabiocody@icloud.com>
2018-07-07 16:40:38 +0200

    Remove cancel from 'Vuoi continuare?' prompt

--- src/main/java/it/polimi/ingsw/client/gui/ClientGUI.java
+++ src/main/java/it/polimi/ingsw/client/gui/ClientGUI.java
@@ -858,7 +858,7 @@ public class ClientGUI extends Application implements Observer {
                 if(requiredData.get(JsonFields.DATA).getAsJsonObject().has(JsonFields.STOP)) {
                     Platform.runLater(() -> {
                         TwoOptionsAlert continueAlert = new TwoOptionsAlert();
-                        Options answer = continueAlert.present("Vuoi continuare?", Options.YES, Options.NO, e -> cancelAction(true, true));
+                        Options answer = continueAlert.present("Vuoi continuare?", Options.YES, Options.NO, null);
                         stop = answer == Options.NO;
                     });
                     while (stop == null)
